e784d695fadc7dda1fe572ee386269e423ff386a
myrean <dhughes1@uci.edu>
2019-04-08 20:50:31 -0700

    removed comments for models

--- models/family.js
+++ models/family.js
@@ -1,16 +1,16 @@
-// module.exports = function (sequelize, DataTypes) {
-//     const Family = sequelize.define("Family", {
-//         id:{type: DataTypes.STRING, primaryKey: true },
-//         familyName: DataTypes.STRING,
-//         accessCode: DataTypes.INTEGER,
-//         numuUsers: DataTypes.INTEGER,
-//         creator: DataTypes.STRING,
-//         createdAt: DataTypes.DATE,
-//         updatedAt: DataTypes.DATE
-//     },
-//     {
-//     freezeTableName: true
-//     });
+module.exports = function (sequelize, DataTypes) {
+    const Family = sequelize.define("Family", {
+        id:{type: DataTypes.STRING, primaryKey: true },
+        familyName: DataTypes.STRING,
+        accessCode: DataTypes.INTEGER,
+        numuUsers: DataTypes.INTEGER,
+        creator: DataTypes.STRING,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+    },
+    {
+    freezeTableName: true
+    });
 
-// return Family;
-// };
+return Family;
+};
--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,10 @@
-module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-        name: DataTypes.STRING,
-        address: DataTypes.STRING,
-        phone_number: DataTypes.STRING,
-        email: DataTypes.STRING
-    });
+// module.exports = function (sequelize, DataTypes) {
+//     const User = sequelize.define("User", {
+//         name: DataTypes.STRING,
+//         address: DataTypes.STRING,
+//         phone_number: DataTypes.STRING,
+//         email: DataTypes.STRING
+//     });
 
-return User;
-};
+// return User;
+// };
