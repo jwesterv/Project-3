49eda5c8192a0f51d2e1f4f5d96b54fa4f87bf99
James Westervelt <james.a.westervelt@gmail.com>
2019-04-15 18:36:30 -0700

    staged changes on VS code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Main from "./pages/Main/Main";
 import About from "./pages/About/About";
 import Chat from "./pages/Chat"
@@ -12,6 +12,7 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route exact path="/about" component={About} />
         <Route exact path="/chat" component={Chat} />
+
       </Router>
     );
   }
