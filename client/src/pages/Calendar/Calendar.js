ed49d746d815d3a9e872d7e2adccbd5e74e8d2c1
Joanna K <44126368+joannakhut@users.noreply.github.com>
2019-04-15 21:06:15 -0700

    stage

--- client/src/pages/Calendar/Calendar.js
+++ client/src/pages/Calendar/Calendar.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+import Calendar from "../../components/Calendar"
+
+import "./style.css"
+
+
 function Calendar() {
     return (
-        <div>
-            
-        </div>
+        <div className="Calendar">
+        <header>
+          <div id="logo">
+            {/* <span className="icon">date_range</span> */}
+            <span>
+              FAM.<b>ily</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+        
+      </div>
     )
 }
 
 export default Calendar;
+
